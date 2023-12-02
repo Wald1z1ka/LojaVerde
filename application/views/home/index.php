e9f0e8c07a054a9aa91dbdd1c9f4f036ed411fab
wald1z1ka <corporativo159852@gmail.com>
2023-12-01 22:34:11 -0400

    meu projeto Loja Verde

---
+++ application/views/home/index.php
@@ -0,0 +1,14 @@
+<?php
+$base = __DIR__;
+include $base .'\..\layout\menu.php';
+//debug_print_backtrace();
+ ?>
+<html>
+<head>
+
+</head>
+<body>
+    <h1> Bem-Vindo </h1>
+    <hr />
+</body>
+</html>
